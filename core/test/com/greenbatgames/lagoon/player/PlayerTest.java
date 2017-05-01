b90273daa95881d408345fd12b7b85abb96c685d
Mike <desrochers.m.j@gmail.com>
2017-05-01 16:18:28 -0600

    Add base for testing methods to be implemented later

--- core/test/com/greenbatgames/lagoon/player/PlayerTest.java
+++ core/test/com/greenbatgames/lagoon/player/PlayerTest.java
@@ -1,19 +1,17 @@
 package com.greenbatgames.lagoon.player;
 
-import com.greenbatgames.lagoon.LagoonGame;
-import com.greenbatgames.lagoon.screen.GameScreen;
-import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 /**
- * Created by Quiv on 15-03-2017.
+ * Created by Quiv on 26-04-2017.
  */
 public class PlayerTest {
 
     @Test
     public void thisAlwaysPasses() throws Exception {
-        assertTrue(true);
+
     }
+
 }
