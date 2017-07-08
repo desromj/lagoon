a011e76bd29cf2e8f0e3a44f4c9965492db74b4c
Mike <desrochers.m.j@gmail.com>
2017-07-08 13:12:50 -0600

    Add vector and angle conversion methods

--- core/src/com/greenbatgames/lagoon/util/Utils.java
+++ core/src/com/greenbatgames/lagoon/util/Utils.java
@@ -1,11 +1,22 @@
 package com.greenbatgames.lagoon.util;
 
-/**
- * Created by Quiv on 02-05-2017.
- */
+import com.badlogic.gdx.math.Vector2;
+
 public class Utils {
 
     public static boolean almostEqualTo (float first, float second, float variance) {
         return Math.abs(first - second) < variance;
+    }
+
+
+    public static float vectorTongle(Vector2 vector) {
+        return (float) Math.atan2(-vector.x, vector.y);
+    }
+
+
+    public static Vector2 angleToVector(Vector2 outVector, float angle) {
+        outVector.x = (float) -Math.sin(angle);
+        outVector.y = (float) Math.cos(angle);
+        return outVector;
     }
 }
